bbf554e6409b1359eaecb0bbd17e505cb2aa5c1a
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-06-20 10:40:39 +0545

    Update build_docker.groovy

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -1,1 +1,26 @@
-hey
+def checkoutRepo() {
+    checkout scm: [$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: 'https://github.com/sanish11/DockerBanking.git']]]
+}
+
+def dockerLogin() {
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
+        bat "docker login -u %DOCKER_HUB_USR% -p %DOCKER_HUB_PSW%"
+    }
+}
+
+def buildAndPushBackendImage() {
+    dir('src/BankingSystemBackend') {
+        def backendImage = docker.build("sanish1212/bankingsystembackend:${env.BUILD_NUMBER}")
+        bat "docker push sanish1212/bankingsystembackend:${env.BUILD_NUMBER}"
+        bat "docker push sanish1212/bankingsystembackend:latest"
+    }
+}
+
+def buildAndPushFrontendImage() {
+    dir('src/BankingSystemFrontend') {
+        def frontendImage = docker.build("sanish1212/bankingsystemfrontend:${env.BUILD_NUMBER}")
+        bat "docker push sanish1212/bankingsystemfrontend:${env.BUILD_NUMBER}"
+        bat "docker push sanish1212/bankingsystemfrontend:latest"
+    }
+}
+
