434561666263f9a111fb7f23e052ecdcb56a9384
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-06-20 10:56:58 +0545

    Update build_docker.groovy

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -1,14 +1,19 @@
-def checkoutRepo(branch, repoUrl) {
+println "build_docker.groovy loaded"
+
+def checkoutRepo(String branch, String repoUrl) {
+    println "Executing checkoutRepo"
     checkout scm: [$class: 'GitSCM', branches: [[name: branch]], userRemoteConfigs: [[url: repoUrl]]]
 }
 
-def dockerLogin(credentialsId) {
+def dockerLogin(String credentialsId) {
+    println "Executing dockerLogin"
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
         bat "docker login -u %DOCKER_HUB_USR% -p %DOCKER_HUB_PSW%"
     }
 }
 
-def buildAndPushImage(directory, imageName, buildNumber, tag = 'latest') {
+def buildAndPushImage(String directory, String imageName, String buildNumber, String tag = 'latest') {
+    println "Executing buildAndPushImage in directory: ${directory}, imageName: ${imageName}, buildNumber: ${buildNumber}, tag: ${tag}"
     dir(directory) {
         def image = docker.build("${imageName}:${buildNumber}")
         bat "docker push ${imageName}:${buildNumber}"
