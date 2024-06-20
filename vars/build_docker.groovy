baeceb5f2d21164cc842e242e23e942823b813a0
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-06-20 10:47:32 +0545

    Update build_docker.groovy
    updated

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -1,26 +1,17 @@
-def checkoutRepo() {
-    checkout scm: [$class: 'GitSCM', branches: [[name: 'main']], userRemoteConfigs: [[url: 'https://github.com/sanish11/DockerBanking.git']]]
+def checkoutRepo(branch, repoUrl) {
+    checkout scm: [$class: 'GitSCM', branches: [[name: branch]], userRemoteConfigs: [[url: repoUrl]]]
 }
 
-def dockerLogin() {
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
+def dockerLogin(credentialsId) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_HUB_USR', passwordVariable: 'DOCKER_HUB_PSW')]) {
         bat "docker login -u %DOCKER_HUB_USR% -p %DOCKER_HUB_PSW%"
     }
 }
 
-def buildAndPushBackendImage() {
-    dir('src/BankingSystemBackend') {
-        def backendImage = docker.build("sanish1212/bankingsystembackend:${env.BUILD_NUMBER}")
-        bat "docker push sanish1212/bankingsystembackend:${env.BUILD_NUMBER}"
-        bat "docker push sanish1212/bankingsystembackend:latest"
+def buildAndPushImage(directory, imageName, buildNumber, tag = 'latest') {
+    dir(directory) {
+        def image = docker.build("${imageName}:${buildNumber}")
+        bat "docker push ${imageName}:${buildNumber}"
+        bat "docker push ${imageName}:${tag}"
     }
 }
-
-def buildAndPushFrontendImage() {
-    dir('src/BankingSystemFrontend') {
-        def frontendImage = docker.build("sanish1212/bankingsystemfrontend:${env.BUILD_NUMBER}")
-        bat "docker push sanish1212/bankingsystemfrontend:${env.BUILD_NUMBER}"
-        bat "docker push sanish1212/bankingsystemfrontend:latest"
-    }
-}
-
