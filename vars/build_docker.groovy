ced1d3c12ebaba87b0b138a50acaeeb3b82424cc
sanish11 <59250164+sanish11@users.noreply.github.com>
2024-07-01 19:09:32 +0545

    Update build_docker.groovy

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -16,7 +16,7 @@ def buildAndPush(String directory, String imageName, String tag) {
     def buildNumber = env.BUILD_NUMBER
     println "Executing buildAndPushImage in directory: ${directory}, imageName: ${imageName}, buildNumber: ${buildNumber}, tag: ${tag}"
     dir(directory) {
-        bat "docker build -t ${imageName}:${buildNumber} ."
+        bat "docker build -v -t ${imageName}:${buildNumber} ."
         bat "docker tag ${imageName}:${buildNumber} ${imageName}:${tag}"
         bat "docker push ${imageName}:${buildNumber}"
         bat "docker push ${imageName}:${tag}"
